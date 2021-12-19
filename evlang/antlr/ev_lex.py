0b114578011fdbdab8eb73829fcf4c7bd97b8759
Charlene Campbell <95987321+ccRlVDSyBZT1UK@users.noreply.github.com>
2021-12-18 22:55:29 -0500

    base antlr

---
+++ evlang/antlr/ev_lex.py
@@ -0,0 +1,135 @@
+# Generated from grammar/ev_lex.g4 by ANTLR 4.9.3
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\27")
+        buf.write("\u009b\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\3\2\5\2/\n\2\3\2\3")
+        buf.write("\2\7\2\63\n\2\f\2\16\2\66\13\2\3\3\3\3\7\3:\n\3\f\3\16")
+        buf.write("\3=\13\3\3\3\3\3\3\4\3\4\3\4\3\4\6\4E\n\4\r\4\16\4F\3")
+        buf.write("\5\3\5\3\5\3\5\6\5M\n\5\r\5\16\5N\3\6\6\6R\n\6\r\6\16")
+        buf.write("\6S\3\6\5\6W\n\6\3\7\3\7\6\7[\n\7\r\7\16\7\\\3\b\6\b`")
+        buf.write("\n\b\r\b\16\ba\3\b\3\b\7\bf\n\b\f\b\16\bi\13\b\3\b\5\b")
+        buf.write("l\n\b\3\t\3\t\5\tp\n\t\3\t\6\ts\n\t\r\t\16\tt\3\n\3\n")
+        buf.write("\3\n\3\n\3\n\3\n\5\n}\n\n\3\13\3\13\3\f\3\f\3\r\3\r\3")
+        buf.write("\16\3\16\3\17\6\17\u0088\n\17\r\17\16\17\u0089\3\20\3")
+        buf.write("\20\3\20\3\20\3\21\3\21\3\22\3\22\3\23\3\23\3\24\3\24")
+        buf.write("\3\25\3\25\3\26\3\26\2\2\27\3\3\5\4\7\5\t\6\13\7\r\b\17")
+        buf.write("\t\21\n\23\13\25\f\27\r\31\16\33\17\35\20\37\21!\22#\23")
+        buf.write("%\24\'\25)\26+\27\3\2\f\4\2C\\c|\6\2\62;C\\aac|\3\2))")
+        buf.write("\5\2\62;CHch\3\2\62\63\3\2\62;\4\2GGgg\4\2--//\4\2\f\f")
+        buf.write("\17\17\4\2\13\13\"\"\2\u00ad\2\3\3\2\2\2\2\5\3\2\2\2\2")
+        buf.write("\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3")
+        buf.write("\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2")
+        buf.write("\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2")
+        buf.write("\2\2!\3\2\2\2\2#\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2")
+        buf.write("\2\2\2+\3\2\2\2\3.\3\2\2\2\5\67\3\2\2\2\7@\3\2\2\2\tH")
+        buf.write("\3\2\2\2\13Q\3\2\2\2\rX\3\2\2\2\17_\3\2\2\2\21m\3\2\2")
+        buf.write("\2\23|\3\2\2\2\25~\3\2\2\2\27\u0080\3\2\2\2\31\u0082\3")
+        buf.write("\2\2\2\33\u0084\3\2\2\2\35\u0087\3\2\2\2\37\u008b\3\2")
+        buf.write("\2\2!\u008f\3\2\2\2#\u0091\3\2\2\2%\u0093\3\2\2\2\'\u0095")
+        buf.write("\3\2\2\2)\u0097\3\2\2\2+\u0099\3\2\2\2-/\7a\2\2.-\3\2")
+        buf.write("\2\2./\3\2\2\2/\60\3\2\2\2\60\64\t\2\2\2\61\63\t\3\2\2")
+        buf.write("\62\61\3\2\2\2\63\66\3\2\2\2\64\62\3\2\2\2\64\65\3\2\2")
+        buf.write("\2\65\4\3\2\2\2\66\64\3\2\2\2\67;\7)\2\28:\n\4\2\298\3")
+        buf.write("\2\2\2:=\3\2\2\2;9\3\2\2\2;<\3\2\2\2<>\3\2\2\2=;\3\2\2")
+        buf.write("\2>?\7)\2\2?\6\3\2\2\2@A\7\62\2\2AB\7z\2\2BD\3\2\2\2C")
+        buf.write("E\t\5\2\2DC\3\2\2\2EF\3\2\2\2FD\3\2\2\2FG\3\2\2\2G\b\3")
+        buf.write("\2\2\2HI\7\62\2\2IJ\7d\2\2JL\3\2\2\2KM\t\6\2\2LK\3\2\2")
+        buf.write("\2MN\3\2\2\2NL\3\2\2\2NO\3\2\2\2O\n\3\2\2\2PR\t\7\2\2")
+        buf.write("QP\3\2\2\2RS\3\2\2\2SQ\3\2\2\2ST\3\2\2\2TV\3\2\2\2UW\5")
+        buf.write("\r\7\2VU\3\2\2\2VW\3\2\2\2W\f\3\2\2\2XZ\t\b\2\2Y[\t\7")
+        buf.write("\2\2ZY\3\2\2\2[\\\3\2\2\2\\Z\3\2\2\2\\]\3\2\2\2]\16\3")
+        buf.write("\2\2\2^`\t\7\2\2_^\3\2\2\2`a\3\2\2\2a_\3\2\2\2ab\3\2\2")
+        buf.write("\2bc\3\2\2\2cg\7\60\2\2df\t\7\2\2ed\3\2\2\2fi\3\2\2\2")
+        buf.write("ge\3\2\2\2gh\3\2\2\2hk\3\2\2\2ig\3\2\2\2jl\5\21\t\2kj")
+        buf.write("\3\2\2\2kl\3\2\2\2l\20\3\2\2\2mo\t\b\2\2np\t\t\2\2on\3")
+        buf.write("\2\2\2op\3\2\2\2pr\3\2\2\2qs\t\7\2\2rq\3\2\2\2st\3\2\2")
+        buf.write("\2tr\3\2\2\2tu\3\2\2\2u\22\3\2\2\2v}\5!\21\2w}\5#\22\2")
+        buf.write("x}\5%\23\2y}\5\'\24\2z}\5)\25\2{}\5+\26\2|v\3\2\2\2|w")
+        buf.write("\3\2\2\2|x\3\2\2\2|y\3\2\2\2|z\3\2\2\2|{\3\2\2\2}\24\3")
+        buf.write("\2\2\2~\177\7<\2\2\177\26\3\2\2\2\u0080\u0081\7.\2\2\u0081")
+        buf.write("\30\3\2\2\2\u0082\u0083\7*\2\2\u0083\32\3\2\2\2\u0084")
+        buf.write("\u0085\7+\2\2\u0085\34\3\2\2\2\u0086\u0088\t\n\2\2\u0087")
+        buf.write("\u0086\3\2\2\2\u0088\u0089\3\2\2\2\u0089\u0087\3\2\2\2")
+        buf.write("\u0089\u008a\3\2\2\2\u008a\36\3\2\2\2\u008b\u008c\t\13")
+        buf.write("\2\2\u008c\u008d\3\2\2\2\u008d\u008e\b\20\2\2\u008e \3")
+        buf.write("\2\2\2\u008f\u0090\7-\2\2\u0090\"\3\2\2\2\u0091\u0092")
+        buf.write("\7/\2\2\u0092$\3\2\2\2\u0093\u0094\7B\2\2\u0094&\3\2\2")
+        buf.write("\2\u0095\u0096\7%\2\2\u0096(\3\2\2\2\u0097\u0098\7&\2")
+        buf.write("\2\u0098*\3\2\2\2\u0099\u009a\7#\2\2\u009a,\3\2\2\2\22")
+        buf.write("\2.\64;FNSV\\agkot|\u0089\3\b\2\2")
+        return buf.getvalue()
+
+
+class ev_lex(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    COMMENTS = 2
+
+    IDENTIFIER = 1
+    STRING_LIT = 2
+    HEX_INT_LIT = 3
+    BIN_INT_LIT = 4
+    DECIMAL_INT_LIT = 5
+    DECIMAL_EXPONENT = 6
+    DECIMAL_FLOAT_LIT = 7
+    DECIMAL_FLOAT_EXPONENT = 8
+    OPERATOR = 9
+    COLON = 10
+    COMMA = 11
+    PAREN_LEFT = 12
+    PAREN_RIGHT = 13
+    EOL = 14
+    WS = 15
+    PLUS = 16
+    MINUS = 17
+    AT = 18
+    HASHTAG = 19
+    DOLLAR = 20
+    EXCLAM = 21
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN", u"COMMENTS" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "':'", "','", "'('", "')'", "'+'", "'-'", "'@'", "'#'", "'$'", 
+            "'!'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "IDENTIFIER", "STRING_LIT", "HEX_INT_LIT", "BIN_INT_LIT", "DECIMAL_INT_LIT", 
+            "DECIMAL_EXPONENT", "DECIMAL_FLOAT_LIT", "DECIMAL_FLOAT_EXPONENT", 
+            "OPERATOR", "COLON", "COMMA", "PAREN_LEFT", "PAREN_RIGHT", "EOL", 
+            "WS", "PLUS", "MINUS", "AT", "HASHTAG", "DOLLAR", "EXCLAM" ]
+
+    ruleNames = [ "IDENTIFIER", "STRING_LIT", "HEX_INT_LIT", "BIN_INT_LIT", 
+                  "DECIMAL_INT_LIT", "DECIMAL_EXPONENT", "DECIMAL_FLOAT_LIT", 
+                  "DECIMAL_FLOAT_EXPONENT", "OPERATOR", "COLON", "COMMA", 
+                  "PAREN_LEFT", "PAREN_RIGHT", "EOL", "WS", "PLUS", "MINUS", 
+                  "AT", "HASHTAG", "DOLLAR", "EXCLAM" ]
+
+    grammarFileName = "ev_lex.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.9.3")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
